faf71f2845e13555f8ce8f65aa0a9401bf895bc7
Audrey <audreyleow@yahoo.com.sg>
2023-01-16 05:49:06 +0800

    overseas nav bar

--- components/overseas/overseas-nav.tsx
+++ components/overseas/overseas-nav.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 import { figtree } from "../utils/font";
 import styles from "./overseas.module.css";
 
@@ -11,55 +12,66 @@ const OverseasNav = ({
 }) => {
   return (
     <div className={styles["overseas-nav"]}>
-      <div
-        className={`${styles.country} ${
-          country === "France" && styles["selected-country"]
-        }`}
-        onClick={() => {
-          onChange("France");
-        }}
-      >
-        France
+      <div className={styles["countries-heading"]}>
+        Countries
+        <Image
+          src={`/assets/overseas/divider.png`}
+          width={125}
+          height={30}
+          alt="divider"
+        />
       </div>
-      <div
-        className={`${styles.country} ${
-          country === "Germany" && styles["selected-country"]
-        }`}
-        onClick={() => {
-          onChange("Germany");
-        }}
-      >
-        Germany
-      </div>
-      <div
-        className={`${styles.country} ${
-          country === "Italy" && styles["selected-country"]
-        }`}
-        onClick={() => {
-          onChange("Italy");
-        }}
-      >
-        Italy
-      </div>
-      <div
-        className={`${styles.country} ${
-          country === "Malaysia" && styles["selected-country"]
-        }`}
-        onClick={() => {
-          onChange("Malaysia");
-        }}
-      >
-        Malaysia
-      </div>
-      <div
-        className={`${styles["last-country"]} ${
-          country === "Vietnam" && styles["selected-country"]
-        }`}
-        onClick={() => {
-          onChange("Vietnam");
-        }}
-      >
-        Vietnam
+      <div className={styles["full-countries"]}>
+        <div
+          className={`${styles.country} ${
+            country === "France" && styles["selected-country"]
+          }`}
+          onClick={() => {
+            onChange("France");
+          }}
+        >
+          France
+        </div>
+        <div
+          className={`${styles.country} ${
+            country === "Germany" && styles["selected-country"]
+          }`}
+          onClick={() => {
+            onChange("Germany");
+          }}
+        >
+          Germany
+        </div>
+        <div
+          className={`${styles.country} ${
+            country === "Italy" && styles["selected-country"]
+          }`}
+          onClick={() => {
+            onChange("Italy");
+          }}
+        >
+          Italy
+        </div>
+        <div
+          className={`${styles.country} ${
+            country === "Malaysia" && styles["selected-country"]
+          }`}
+          onClick={() => {
+            onChange("Malaysia");
+          }}
+        >
+          Malaysia
+        </div>
+        <div
+          className={`${styles["last-country"]} ${
+            country === "Vietnam" && styles["selected-country"]
+          }`}
+          onClick={() => {
+            onChange("Vietnam");
+          }}
+        >
+          Vietnam
+        </div>
       </div>
     </div>
   );
--- pages/overseas/index.tsx
+++ pages/overseas/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Container from "../../components/container";
 import MoreOverseasStories from "../../components/overseas/overseas-more-stories";
 import OverseasHeroPost from "../../components/overseas/overseas-hero-post";
@@ -9,6 +9,7 @@ import {
   overseasOptions,
   tagsOptions,
 } from "../../components/food/filter-options";
+import PostPreview from "../../components/main-page/post-preview";
 import OverseasNav from "../../components/overseas/overseas-nav";
 import OverseasNewestPosts from "../../components/overseas/overseas-newest-posts";
 import styles from "../../components/food/food.module.css";
@@ -31,6 +32,13 @@ export default function Index({ allOverseasPosts }: Props) {
   const newPosts = filteredOverseasPosts.slice(0, 5);
   const [selectedCountry, setSelectedCountry] = useState("France");
 
+  useEffect(() => {
+    let newFilteredOverseasPosts = allOverseasPosts.filter((post) =>
+      post.country.includes(selectedCountry)
+    );
+    setFilteredOverseasPosts(newFilteredOverseasPosts);
+  }, [selectedCountry]);
+
   return (
     <>
       <Layout>
@@ -38,12 +46,31 @@ export default function Index({ allOverseasPosts }: Props) {
           <title>AudreyTheFoodie</title>
         </Head>
         <Container>
-          {/* <OverseasNav
+          <OverseasNav
             country={selectedCountry}
             onChange={setSelectedCountry}
           />
-          <OverseasNewestPosts posts={newPosts} country={selectedCountry} /> */}
-          {heroPost && (
+          <OverseasNewestPosts posts={newPosts} country={selectedCountry} />
+          <div className={styles["food-container"]}>
+            <div className={styles["main-reviews"]}>
+              {filteredOverseasPosts.map((post) => (
+                <PostPreview
+                  key={post.slug}
+                  title={post.title}
+                  coverImage={post.coverImage}
+                  date={post.date}
+                  zone={post.country}
+                  slug={`/posts/${post.slug}`}
+                  excerpt={post.excerpt}
+                  nearestMRT={post.nearestMRT}
+                  location={post.location}
+                  rating={post.rating}
+                  collab={post.collab}
+                />
+              ))}
+            </div>
+          </div>
+          {/* {heroPost && (
             <OverseasHeroPost
               title={heroPost.title}
               coverImage={heroPost.coverImage}
@@ -53,7 +80,7 @@ export default function Index({ allOverseasPosts }: Props) {
               excerpt={heroPost.excerpt}
             />
           )}
-          {morePosts.length > 0 && <MoreOverseasStories posts={morePosts} />}
+          {morePosts.length > 0 && <MoreOverseasStories posts={morePosts} />} */}
         </Container>
       </Layout>
     </>
